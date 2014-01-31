a99f278751f7d227aad5dc74419c71b7e80c7ff8
lupusdei <lupusdei108@gmail.com>
2014-01-31 15:36:17 -0500

    Initial Commit

---
+++ DynamicsPlayground/JMMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMMViewController.h
+//  DynamicsPlayground
+//
+//  Created by Justin Martin on 1/31/14.
+//  Copyright (c) 2014 JMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMMViewController : UIViewController
+
+@end
---
+++ DynamicsPlayground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DynamicsPlayground
+//
+//  Created by Justin Martin on 1/31/14.
+//  Copyright (c) 2014 JMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMMAppDelegate class]));
+    }
+}
